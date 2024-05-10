1c0380dc421c76a5717e5fb49523581ce9988908
James Swent <jake@osprey21.com>
2024-05-10 12:16:16 -0400

    feat(media-room): Add source control to Media Room API

--- crestron_bridge/web/api/mediaRoom/schema.py
+++ crestron_bridge/web/api/mediaRoom/schema.py
@@ -3,10 +3,15 @@ from pydantic import BaseModel
 class MediaRoomPost(BaseModel):
   status: str
 
+class MediaRoomSelectSourcePost(BaseModel):
+  source: str
+
 class MediaRoomPostResponse(BaseModel):
   status: str
+  source: str | None
   response: str
 
 class MediaRoomGetResponse(BaseModel):
   status: str
+  source: str
   is_active: str
--- crestron_bridge/web/api/mediaRoom/views.py
+++ crestron_bridge/web/api/mediaRoom/views.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from crestron_bridge.web.api.mediaRoom.schema import MediaRoomPost, MediaRoomPostResponse, MediaRoomGetResponse
+from crestron_bridge.web.api.mediaRoom.schema import MediaRoomPost, MediaRoomSelectSourcePost, MediaRoomPostResponse, MediaRoomGetResponse
 from crestron_bridge.services.telnet.lifetime import get_telnet_manager
+from crestron_bridge.services.state.lifetime import get_server_state
 
 router = APIRouter()
 tm = get_telnet_manager()
+state = get_server_state()
 
 @router.post("/", response_model=MediaRoomPost)
 async def update_media_room_status(
@@ -26,12 +28,42 @@ async def update_media_room_status(
 
 @router.get("/", response_model=MediaRoomGetResponse)
 async def get_media_room_status() -> MediaRoomGetResponse:
-  response = tm.send_command("MEDIA ROOM STATUS")
+  print(f"Getting media room status from state")
+  media_state = state.get_media_room_state()
+  return MediaRoomGetResponse(status=media_state.status, source=media_state.source, is_active=media_state.is_active)
+
+@router.post("/turn-on", response_model=MediaRoomPostResponse)
+async def turn_on():
+  response = tm.send_command("MEDIA ROOM ON")
   print(response)
-  
-  if response.__contains__("MEDIA ROOM STATUS ON"):
-    return MediaRoomGetResponse(status="ON", is_active="true")
-  elif response.__contains__("MEDIA ROOM STATUS POWERING UP"):
-    return MediaRoomGetResponse(status="ON", is_active="true")
-  else:
-    return MediaRoomGetResponse(status="OFF", is_active="false")
+
+  response_text = response.upper()
+  if "MEDIA ROOM ON OK" in response_text:
+    state.update_media_room_state(status="ON", is_active="true")
+    return MediaRoomPostResponse(status="POWERING UP", response="OK")
+  return MediaRoomPostResponse(status="ERROR", response="ERROR")
+
+@router.post("/turn-off", response_model=MediaRoomPostResponse)
+async def turn_off():
+  response = tm.send_command("MEDIA ROOM OFF")
+  print(response)
+
+  response_text = response.upper()
+  if "MEDIA ROOM OFF OK" in response_text:
+    state.update_media_room_state(status="OFF", is_active="false")
+    return MediaRoomPostResponse(status="POWERING DOWN", response="OK")
+  return MediaRoomPostResponse(status="ERROR", response="ERROR")
+
+@router.post("/select-source", response_model=MediaRoomPostResponse)
+async def select_source(
+    body: MediaRoomSelectSourcePost,
+):
+  response = tm.send_command(f"MEDIA ROOM SRC {body.source}")
+  print(response)
+
+  response_text = response.upper()
+  if f"MEDIA ROOM SRC {body.source} OK" in response_text:
+    state.update_media_room_state(source=body.source)
+    media_state = state.get_media_room_state()
+    return MediaRoomPostResponse(status=media_state.status, source=body.source, response="OK")
+  return MediaRoomPostResponse(status="ERROR", source="ERROR", response="ERROR")
