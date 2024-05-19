e6cbea91fab72720b0507e1381e3bd0a420a1703
James Swent <jake@osprey21.com>
2024-05-19 17:09:48 -0400

    feat(audio): Add new audio locations

--- crestron_bridge/web/api/audio/views.py
+++ crestron_bridge/web/api/audio/views.py
@@ -7,3 +7,6 @@ router = APIRouter()
 
 router.include_router(deck.router, prefix="/deck", tags=["deck"])
 router.include_router(AudioLocation("KITCHEN").router, prefix="/kitchen", tags=["kitchen"])
+router.include_router(AudioLocation("DINING ROOM").router, prefix="/dining-room", tags=["dining-room"])
+router.include_router(AudioLocation("GAME ROOM").router, prefix="/game-room", tags=["game-room"])
+router.include_router(AudioLocation("JAKE OFFICE").router, prefix="/jake-office", tags=["jake-office"])
